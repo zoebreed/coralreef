c28a0f531480607bd4a1005b203a1f6c1a7d4586
amarisesilie <91152459+amarisesilie@users.noreply.github.com>
2021-11-20 22:12:28 +0100

    added temperature
    this causes an error that I will look into (you can still run the code)

---
+++ coralVR/Assets/Scripts/temperatureChange.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class temperatureChange : MonoBehaviour
+{
+    public Text temperatureText;
+    public float currentTemperature;
+    public bool beginState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beginState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (beginState = true && currentTemperature > 10.0f)
+        {
+            currentTemperature -= 0.1f;
+        }
+
+        if (beginState = false && currentTemperature < 35.0f)
+        {
+            currentTemperature += 0.1f;
+        }
+
+        // currentTemperature;
+        temperatureText.text = currentTemperature.ToString("F1") + "°C";
+    }
+}
